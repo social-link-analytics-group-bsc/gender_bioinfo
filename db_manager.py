d605cb4d6ee3b7e161b435ee36ad214b9fe73b93
Jorge Saldivar <Jorge Saldivar>
2019-02-25 15:18:04 +0100

    Add a class to manage the database used to store the extracted data

---
+++ db_manager.py
@@ -0,0 +1,53 @@
+
+from pymongo import MongoClient
+from utils import get_config
+
+import logging
+import pathlib
+
+logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('gender_identification.log')),
+                    level=logging.DEBUG)
+
+
+class DBManager:
+    __db = None
+    __host = None
+    __collection = ''
+
+    def __init__(self, collection, db_name=''):
+        current_dir = pathlib.Path(__file__).parents[0]
+        config_fn = current_dir.joinpath('config.json')
+        config = get_config(config_fn)
+        self.__host = config['mongo']['host']
+        self.__port = config['mongo']['port']
+        client = MongoClient(self.__host + ':' + self.__port)
+
+        if not db_name:
+            self.__db = client[config['mongo']['db_name']]
+        else:
+            self.__db = client[db_name]
+        self.__collection = collection
+
+    def save_record(self, record_to_save):
+        self.__db[self.__collection].insert(record_to_save)
+
+    def find_record(self, query):
+        return self.__db[self.__collection].find_one(query)
+
+    def update_record(self, filter_query, new_values, create_if_doesnt_exist=False):
+        return self.__db[self.__collection].update_one(filter_query, {'$set': new_values},
+                                                       upsert=create_if_doesnt_exist)
+
+    def search(self, query):
+        return self.__db[self.__collection].find(query, no_cursor_timeout=True)
+
+    def store_record(self, record_to_store):
+        record_identifier = record_to_store['DOI']
+        num_results = self.search({'DOI': record_identifier}).count()
+        if num_results == 0:
+            self.save_record(record_to_store)
+            logging.info(f"Inserted record with DOI: {record_identifier}")
+            return True
+        else:
+            logging.info(f"Found record duplicated DOI: {record_identifier}")
+            return False
