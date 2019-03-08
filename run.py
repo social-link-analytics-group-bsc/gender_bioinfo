c3ebe3b53703f650f4c9e771c2447a4ad4159cf9
Jorge Saldivar <Jorge Saldivar>
2019-03-08 18:04:03 +0100

    Add the call to the method to get the authors of the ncbi papers

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
-from data_extractor import extra_data_untrackable_journals, obtain_author_gender
-from data_loader import load_data_from_file_into_db
+from data_extractor import extra_data_untrackable_journals, obtain_author_gender, get_authors_ncbi_journal
+from data_loader import load_data_from_file_into_db, update_data_from_file
 from data_wrangler import create_paper_authors_collection, compute_authors_h_index
 from db_manager import DBManager
 
@@ -13,8 +13,10 @@ logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('gen
 
 if __name__ == '__main__':
     db = DBManager('bioinfo_papers')
-    # load_data_from_file_into_db(db, 'biolitmap_data.csv')
+    # load_data_from_file_into_db('biolitmap_data.csv')
     # extra_data_untrackable_journals(db)
     # obtain_author_gender(db)
     # create_paper_authors_collection(db)
-    compute_authors_h_index()
+    # compute_authors_h_index()
+    # update_data_from_file('genero_journals.csv')
+    get_authors_ncbi_journal(db)
