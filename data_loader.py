1e2997d3a3688527e7055fee2e5e603343692487
Jorge Saldivar <Jorge Saldivar>
2019-03-08 18:03:44 +0100

    Add a new method to update the papers' authors

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,16 @@
+from db_manager import DBManager
+
+import ast
 import csv
+import logging
 import pathlib
 
+logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('gender_identification.log')),
+                    level=logging.DEBUG)
 
-def load_data_from_file_into_db(db, filename):
+
+def load_data_from_file_into_db(filename):
+    db = DBManager('bioinfo_papers')
     current_dir = pathlib.Path(__file__).parents[0]
     bio_file_name = current_dir.joinpath('data', filename)
     with open(str(bio_file_name), 'r', encoding='ISO-8859-1') as f:
@@ -10,3 +18,23 @@ def load_data_from_file_into_db(db, filename):
         for line in file:
             line['source'] = line['source'].lower()
             db.store_record(line)
+
+
+def update_data_from_file(filename):
+    db = DBManager('bioinfo_papers')
+    current_dir = pathlib.Path(__file__).parents[0]
+    bio_file_name = current_dir.joinpath('data', filename)
+    with open(str(bio_file_name), 'r', encoding='ISO-8859-1') as f:
+        file = csv.DictReader(f, delimiter=',')
+        for line in file:
+            paper = db.find_record({'DOI': line['DOI']})
+            if not paper or 'authors' not in paper.keys():
+                logging.info(f"Updating the paper {line['DOI']}")
+                line['source'] = line['source'].lower()
+                try:
+                    line['authors'] = ast.literal_eval(line['authors_fullname'])
+                    del line['authors_fullname']
+                except:
+                    logging.error(f"Could not update the paper {line['DOI']}")
+                db.update_record({'DOI': line['DOI']}, line, create_if_doesnt_exist=True)
+
