775ea91eb42d2c8166919cf8278449cf51258847
Jorge Saldivar <Jorge Saldivar>
2019-03-18 17:59:59 +0100

    Add the call to a new data extraction method

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 
-from data_extractor import extra_data_untrackable_journals, obtain_author_gender, get_authors_ncbi_journal
+from data_extractor import extra_data_untrackable_journals, obtain_author_gender, get_authors_ncbi_journal, \
+                           obtain_author_affiliation
 from data_loader import load_data_from_file_into_db, update_data_from_file
 from data_wrangler import create_update_paper_authors_collection, compute_authors_h_index
 from db_manager import DBManager
@@ -12,7 +13,8 @@ logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('gen
 
 
 if __name__ == '__main__':
-    db = DBManager('bioinfo_papers')
+    db_papers = DBManager('bioinfo_papers')
+    db_authors = DBManager('bioinfo_authors')
     # load_data_from_file_into_db('biolitmap_data.csv')
     # extra_data_untrackable_journals(db)
     # create_paper_authors_collection(db)
@@ -20,5 +22,6 @@ if __name__ == '__main__':
     # update_data_from_file('genero_journals.csv')
     # get_authors_ncbi_journal(db)
     # obtain_author_gender(db)
-    create_update_paper_authors_collection(db)
-    compute_authors_h_index()
+    # create_update_paper_authors_collection(db)
+    # compute_authors_h_index()
+    obtain_author_affiliation(db_papers, db_authors)
