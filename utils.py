2688cb5b377b2a7bbb849ba76de9e2c9fe5c0d35
Jorge Saldivar <Jorge Saldivar>
2019-03-28 17:54:49 +0100

    Move curation fucntions to utils and add references in data_extractor

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 
 import json
+import re
+
 
 # Get configuration from file
 def get_config(config_file):
@@ -7,3 +9,11 @@ def get_config(config_file):
         config = json.loads(f.read())
     return config
 
+
+def curate_author_name(author_raw):
+    regex = re.compile('[0-9*]')
+    return regex.sub('', author_raw).replace(' and ', ' ').strip().rstrip(',').lstrip(',')
+
+
+def curate_affiliation_name(affiliation_raw):
+    return affiliation_raw.replace(' and ', ' ').strip().rstrip(',').lstrip(',')
